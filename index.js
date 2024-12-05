9dedb519450bf7f8b59160c45c494c42e9325b9b
KI JONG BYUN <kjbyun0@gmail.com>
2024-12-05 15:22:17 -0600

    Done

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]/g, "");
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.split(" ");
+    return words.map((word, i) => {
+      const wordInLowCase = word.toLowerCase();
+      if (i !== 0 && exceptions.includes(wordInLowCase)) {
+        return wordInLowCase;
+      } else {
+        return this.capitalize(word);
+      }
+    }).join(" ");
+  }
 }
